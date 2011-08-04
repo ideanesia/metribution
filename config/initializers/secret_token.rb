100adda819faf0e4afbee3605919b49a1b65069d
Anton Ideanesia <anton@ideanesia.com>
2011-08-04 10:33:56 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metribution::Application.config.secret_token = '1cbb61a413c7292f8a9949d3367665011ccdf4d75e4fd201ef36b0d0db56489f237051544fc8912c130458ffbfedd3fd3f339be99d08b1f6dd004ae04b4a454a'
